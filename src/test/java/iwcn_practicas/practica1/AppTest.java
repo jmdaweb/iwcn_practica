c5d80fdedec07ec9a03116f85ffeacad7b6bd359
Jose Manuel Delicado <jmdaweb@hotmail.com>
2016-12-07 19:14:01 +0100

    Soporte inicial para los tests

--- src/test/java/iwcn_practicas/practica1/AppTest.java
+++ src/test/java/iwcn_practicas/practica1/AppTest.java
@@ -1,38 +1,45 @@
 package iwcn_practicas.practica1;
+import java.io.File;
+import java.util.concurrent.TimeUnit;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-/**
- * Unit test for simple App.
- */
-public class AppTest 
-    extends TestCase
-{
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public AppTest( String testName )
-    {
-        super( testName );
-    }
-
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
-    }
-
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+@WebIntegrationTest
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = SpringMvcThymeleafApp.class)
+public class AppTest{
+	private static final long timeout=30;
+	private WebDriver driver;
+	@BeforeClass
+	public static void setupClass(){
+		System.setProperty("webdriver.chrome.driver", "D:"+File.separator+"chromedriver.exe");
+	}
+	@Before
+	public void setupTest(){
+		driver=new ChromeDriver();
+	}
+	@After
+	public void teardown(){
+		if (driver!=null){
+			driver.quit();
+		}
+	}
+	@Test
+	public void test(){
+		driver.manage().timeouts().implicitlyWait(timeout, TimeUnit.SECONDS);
+		
+	}
 }
